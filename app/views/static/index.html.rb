6ef87561d8692b608947fed77e70073ecfec5669
James Gone <gz5@qq.com>
2016-02-03 22:23:43 +0000

    index.html initial  modified route.rb secret.yml static_controller

---
+++ app/views/static/index.html.rb
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>eim studio</title>
+  </head>
+  <body>
+    <p>eim studio for web application </p> 
+  </body>
+
+</html>
+
+
